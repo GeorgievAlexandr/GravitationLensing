750a852c84fb1fd52b6a850fe7df54a184bbb46f
aleksandrgeorgiev <geosasha.mail@gmail.com>
2025-06-29 18:40:48 +0300

    Initial Commit

---
+++ gravitationLensing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gravitationLensing
+//
+//  Created by Александр Георгиев on 29.06.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
